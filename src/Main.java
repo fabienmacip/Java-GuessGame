d24581db1bae04cc1faf5548e2a18d771b379c78
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-05-24 12:19:34 +0200

    Rejouer après une fin de partie

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static char scanLetter() {
+    private static char scanLetter(String question) {
         final var scanner = new Scanner(System.in);
         Character letter = null;
         do {
-            System.out.println("Entrez une lettre : ");
+            System.out.println(question);
             var input = scanner.nextLine();
             if (input.length() == 1) {
                 letter = input.charAt(0);
@@ -22,27 +22,35 @@ public class Main {
 
         final var random = new Random();
         final var words = "belgique france animal perruche perruque limitation voiture automobile ananas".split(" ");
-        final var wordToGuess = words[random.nextInt(words.length)];
-        final var game = new GuessGame(wordToGuess, 10);
+        var wordToGuess = words[random.nextInt(words.length)];
+        var game = new GuessGame(wordToGuess, 10);
 
         System.out.println("Début du jeu du pendu !");
 
         while(true) {
             System.out.println(game);
-            final var letter = scanLetter();
+            final var letter = scanLetter("Entrez une lettre : ");
 
             game.guessLetter(letter);
 
             if(game.isLost()) {
                 System.out.println(game);
                 System.out.println("Perdu !");
-                break;
             }
 
             if(game.isWon()) {
                 System.out.println(game);
                 System.out.println("Gagné !");
-                break;
+            }
+
+            if(game.isLost() || game.isWon()) {
+                var replayAnswer = scanLetter("Voulez-vous rejouer ? (O/N)");
+                if(replayAnswer == 'y' || replayAnswer == 'Y' || replayAnswer == 'o' || replayAnswer == 'O') {
+                    wordToGuess = words[random.nextInt(words.length)];
+                    game = new GuessGame(wordToGuess, 10);
+                } else {
+                    break;
+                }
             }
         }
 
